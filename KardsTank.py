6b9a8005a7476f106da3a677c8469c73dedc08d0
SaltyFive <xxccyy_aa@163.com>
2025-07-24 17:19:07 +0800

    在KardsTank.py里面用Settings设置

--- KardsTank.py
+++ KardsTank.py
@@ -1,14 +1,14 @@
 import sys
 import pygame
-
+from settings import Settings
 class KardsTank:
     '''管理游戏资源及其行为'''
     def __init__(self):
         '''初始化游戏'''
         pygame.init()
+        self.settings = Settings()
         
-        self.screen = pygame.display.set_mode((1200,800))
-        self.bg_color = 230,230,230
+        self.screen = pygame.display.set_mode(self.settings.screen_size)
         pygame.display.set_caption("这个,,游戏。那。坦克,打4对面?对面。。是,你的机油?用wasd 上下左右。冻起来,然后j和,数字0打.")
         
     def run_game(self):
@@ -17,10 +17,9 @@ class KardsTank:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-        
-        pygame.display.flip()
-        self.screen.fill(self.bg_color)
-        
+            self.screen.fill(self.settings.bg_color)
+            pygame.display.flip()       
+
 if __name__ == '__main__':
     kt = KardsTank()
     kt.run_game()
